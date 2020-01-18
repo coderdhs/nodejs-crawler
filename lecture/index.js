5ceb40d06f157824a726c971e930379c32cd7857
dhs <coderdhs@gmail.com>
2020-01-18 21:08:23 +0900

    2-4 csv에 출력하기

--- lecture/index.js
+++ lecture/index.js
@@ -7,6 +7,7 @@ const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
   try {
+    const result = [];
     const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === "production" });
     // false일때 화면이 보임(개발중), 배포시에는 true
     await Promise.all(
@@ -17,7 +18,8 @@ const crawler = async () => {
           const scoreEl = await page.$(".score.score_left .star_score");
           if (scoreEl) {
             const text = await page.evaluate(tag => tag.textContent, scoreEl);
-            console.log(r[0], "평점", text.trim());
+            // console.log(r[0], "평점", text.trim());
+            result[i] = [r[0], r[1], text.trim()]; // data.csv 순서 그대로 유지
           }
           await page.waitFor(3000);
           await page.close();
@@ -27,6 +29,8 @@ const crawler = async () => {
       })
     );
     await browser.close();
+    const str = stringify(result);
+    fs.writeFileSync("csv/result.csv", str);
   } catch (err) {
     console.error(err);
   }
