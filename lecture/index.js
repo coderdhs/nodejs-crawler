661bc545e3fad322573d0d1c9d326673127f23b1
dhs <coderdhs@gmail.com>
2020-01-18 17:57:56 +0900

    1-8 엑셀에 쓰기

--- lecture/index.js
+++ lecture/index.js
@@ -1,46 +1,48 @@
 const xlsx = require("xlsx");
 const axios = require("axios"); // ajax 라이브러리 : html 요청
 const cheerio = require("cheerio"); // html 파싱
+const add_to_sheet = require("./add_to_sheet");
 
 const workbook = xlsx.readFile("xlsx/data.xlsx");
-for (const name of workbook.Sheets) {
-  const ws = workbook.Sheets[name];
-} // 시트별로 따로 코딩
-const records = xlsx.utils.sheet_to_json(ws, { header: "A" });
-ws["!ref"] = "A2:B11"; // 범위 재설정
+const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws);
 
 /* for (const [i, r] of records.entries()) {
   console.log(i, r.제목, r.링크);
 } */
 
-console.log(records);
+// console.log(records);
 
-// const crawler = async () => {
-//   for (const [i, r] of records.entries()) {
-//     // 순서를 보장. 속도느림.
-//     const response = await axios.get(r.링크);
-//     if (response.status === 200) {
-//       // 응답이 성공한 경우
-//       const html = response.data;
-//       // console.log(html);
-//       const $ = cheerio.load(html);
-//       const text = $(".score.score_left .star_score").text();
-//       console.log(r.제목, "평점", text.trim());
-//     }
-//   }
-//   /*   await Promise.all( // 순서를 보장하지 않음. 속도빠름.
-//     records.map(async r => {
-//       const response = await axios.get(r.링크);
-//       if (response.status === 200) {
-//         // 응답이 성공한 경우
-//         const html = response.data;
-//         // console.log(html);
-//         const $ = cheerio.load(html);
-//         const text = $(".score.score_left .star_score").text();
-//         console.log(r.제목, "평점", text.trim());
-//       }
-//     })
-//   ); */
-// };
+const crawler = async () => {
+  add_to_sheet(ws, "C1", "s", "평점");
+  for (const [i, r] of records.entries()) {
+    // 순서를 보장. 속도느림.
+    const response = await axios.get(r.링크);
+    if (response.status === 200) {
+      // 응답이 성공한 경우
+      const html = response.data;
+      // console.log(html);
+      const $ = cheerio.load(html);
+      const text = $(".score.score_left .star_score").text();
+      console.log(r.제목, "평점", text.trim());
+      const newCell = "C" + (i + 2);
+      add_to_sheet(ws, newCell, "n", parseFloat(text.trim()));
+    }
+  }
+  xlsx.writeFile(workbook, "xlsx/result.xlsx");
+  /*   await Promise.all( // 순서를 보장하지 않음. 속도빠름.
+    records.map(async r => {
+      const response = await axios.get(r.링크);
+      if (response.status === 200) {
+        // 응답이 성공한 경우
+        const html = response.data;
+        // console.log(html);
+        const $ = cheerio.load(html);
+        const text = $(".score.score_left .star_score").text();
+        console.log(r.제목, "평점", text.trim());
+      }
+    })
+  ); */
+};
 
 crawler();
