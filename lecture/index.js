a68e080fbc40e31c7487952469d3e2495ef50d9e
dhs <coderdhs@gmail.com>
2020-01-18 17:04:22 +0900

    1-4 axios, cherrio

--- lecture/index.js
+++ lecture/index.js
@@ -1,15 +1,29 @@
 const xlsx = require("xlsx");
+const axios = require("axios"); // ajax 라이브러리 : html 요청
+const cheerio = require("cheerio"); // html 파싱
 
 const workbook = xlsx.readFile("xlsx/data.xlsx");
-
 const ws = workbook.Sheets.영화목록;
-
 const records = xlsx.utils.sheet_to_json(ws);
 
-/* for (const [i, r] of records.entries()) { // 배열.entries()를 쓰면 내부 배열이  [인덱스, 값] 모양 이터레이터로 바뀜
+/* for (const [i, r] of records.entries()) {
   console.log(i, r.제목, r.링크);
 } */
 
-records.forEach((r, i) => {
-  console.log(i, r.제목, r.링크);
-});
+const crawler = async () => {
+  await Promise.all(
+    records.map(async r => {
+      const response = await axios.get(r.링크);
+      if (response.status === 200) {
+        // 응답이 성공한 경우
+        const html = response.data;
+        // console.log(html);
+        const $ = cheerio.load(html);
+        const text = $(".score.score_left .star_score").text();
+        console.log(r.제목, "평점", text.trim());
+      }
+    })
+  );
+};
+
+crawler();
