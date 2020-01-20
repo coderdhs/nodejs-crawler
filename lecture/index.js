202dd232841890717fc63929c21d9586bb09c7b1
dhs <coderdhs@gmail.com>
2020-01-21 00:20:49 +0900

    5 페이스북 로그인 로그아웃 마우스조작 등등

--- lecture/index.js
+++ lecture/index.js
@@ -1,35 +1,158 @@
 const puppeteer = require("puppeteer");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const crawler = async () => {
   try {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--window-size=1920,1080", "--disable-notifications"]
+    });
     const page = await browser.newPage();
-    await page.goto("https://unsplash.com");
-    let result = [];
-    while (result.length <= 30) {
-      const srcs = await page.evaluate(() => {
-        window.scrollTo(0, 0);
-        let imgs = [];
-        const imgEls = document.querySelectorAll("figure"); // 사이트 바뀌었을 때 클래스 적절히 바꾸기
-        if (imgEls.length) {
-          imgEls.forEach(v => {
-            let src = v.querySelector("img._2zEKz").src;
-            if (src) {
-              imgs.push(src);
-            }
-            v.parentElement.removeChild(v);
-          });
+    await page.setViewport({ width: 1080, height: 1080 });
+    await page.goto("https://facebook.com");
+    // const id = process.env.EMAIL;
+    // const password = process.env.PASSWORD;
+    /* await page.evaluate(
+      (id, password) => {
+        document.querySelector("#email").value = id;
+        document.querySelector("#pass").value = password;
+        document.querySelector("#loginbutton").click();
+      },
+      id,
+      password
+    ); */
+
+    /*  page.on("dialog", async dialog => {
+      console.log(dialog.type(), dialog.message());
+      await dialog.accept(); // accept는 confirm의 확인, dismiss는 취소
+    });
+    
+    await page.evaluate(() => {
+      if (confirm("이 창을 꺼야 다음으로 넘어갑니다.")) {
+        return (location.href = "http://zerocho.com");
+      }
+      return (location.href = "http://inflearn.com");
+    }); */
+
+    await page.evaluate(() => {
+      (() => {
+        const box = document.createElement("div");
+        box.classList.add("mouse-helper");
+        const styleElement = document.createElement("style");
+        styleElement.innerHTML = `
+          .mouse-helper {
+            pointer-events: none;
+            position: absolute;
+            z-index: 100000;
+            top: 0;
+            left: 0;
+            width: 20px;
+            height: 20px;
+            background: rgba(0,0,0,.4);
+            border: 1px solid white;
+            border-radius: 10px;
+            margin-left: -10px;
+            margin-top: -10px;
+            transition: background .2s, border-radius .2s, border-color .2s;
+          }
+          .mouse-helper.button-1 {
+            transition: none;
+            background: rgba(0,0,0,0.9);
+          }
+          .mouse-helper.button-2 {
+            transition: none;
+            border-color: rgba(0,0,255,0.9);
+          }
+          .mouse-helper.button-3 {
+            transition: none;
+            border-radius: 4px;
+          }
+          .mouse-helper.button-4 {
+            transition: none;
+            border-color: rgba(255,0,0,0.9);
+          }
+          .mouse-helper.button-5 {
+            transition: none;
+            border-color: rgba(0,255,0,0.9);
+          }
+          `;
+        document.head.appendChild(styleElement);
+        document.body.appendChild(box);
+        document.addEventListener(
+          "mousemove",
+          event => {
+            box.style.left = event.pageX + "px";
+            box.style.top = event.pageY + "px";
+            updateButtons(event.buttons);
+          },
+          true
+        );
+        document.addEventListener(
+          "mousedown",
+          event => {
+            updateButtons(event.buttons);
+            box.classList.add("button-" + event.which);
+          },
+          true
+        );
+        document.addEventListener(
+          "mouseup",
+          event => {
+            updateButtons(event.buttons);
+            box.classList.remove("button-" + event.which);
+          },
+          true
+        );
+        function updateButtons(buttons) {
+          for (let i = 0; i < 5; i++) box.classList.toggle("button-" + i, !!(buttons & (1 << i)));
         }
-        window.scrollBy(0, 100);
-        setTimeout(() => {
-          window.scrollBy(0, 200);
-        }, 500);
-        return imgs;
-      });
-      result = result.concat(srcs);
-      await page.waitForSelector("figure");
-    }
-    console.log(result);
+      })();
+    });
+
+    /* await page.mouse.move(1000, 45);
+    await page.waitFor(1000);
+    await page.mouse.click(1000, 45); */
+
+    /* await page.mouse.move(50, 50);
+    await page.waitFor(1000);
+    await page.mouse.down();
+    await page.waitFor(1000);
+    await page.mouse.move(1000, 1000);
+    await page.waitFor(1000);
+    await page.mouse.up(); // 드레그 */
+
+    await page.type("#email", process.env.EMAIL);
+    /* await page.click("#email");
+    await page.keyboard.down("ShiftLeft");
+    await page.keyboard.press("KeyZ");
+    await page.waitFor(1000);
+    await page.keyboard.press("KeyE");
+    await page.waitFor(1000);
+    await page.keyboard.press("KeyR");
+    await page.waitFor(1000);
+    await page.keyboard.press("KeyO");
+    await page.waitFor(1000);
+    await page.keyboard.press("KeyC");
+    await page.waitFor(1000);
+    await page.keyboard.press("KeyH");
+    await page.waitFor(1000);
+    await page.keyboard.press("KeyO");
+    await page.waitFor(1000);
+    await page.keyboard.up("ShiftLeft"); //대문자 치기 */
+
+    await page.type("#pass", process.env.PASSWORD);
+    await page.hover("#loginbutton"); // 사람인척..
+    await page.click("#loginbutton");
+    await page.waitForResponse(response => {
+      return response.url().includes("login_attempt");
+    });
+    await page.keyboard.press("Escape");
+    await page.waitFor(3000);
+    await page.click("#userNavigationLabel");
+    await page.waitForSelector("li.navSubmenu:last-child");
+    await page.waitFor(3000);
+    await page.click("li.navSubmenu:last-child");
     await page.close();
     await browser.close();
   } catch (e) {
