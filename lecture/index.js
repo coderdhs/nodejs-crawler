7d798e70a613dfd581b5778a10ed73951330d627
dhs <coderdhs@gmail.com>
2020-01-20 22:04:47 +0900

    4 인피니트 스크롤링 크롤링 (error)

--- lecture/index.js
+++ lecture/index.js
@@ -1,88 +1,39 @@
-const xlsx = require("xlsx");
 const puppeteer = require("puppeteer");
-const axios = require("axios");
-const fs = require("fs");
-const add_to_sheet = require("./add_to_sheet");
-
-const workbook = xlsx.readFile("xlsx/data.xlsx");
-const ws = workbook.Sheets.영화목록;
-const records = xlsx.utils.sheet_to_json(ws);
-
-fs.readdir("screenshot", err => {
-  if (err) {
-    console.error("screenshot folder가 없어 생성함");
-    fs.mkdirSync("screenshot"); // sync 메서드는 프로그램의 처음과 끝에만 쓰기.
-  }
-});
-
-fs.readdir("poster", err => {
-  if (err) {
-    console.error("poster folder가 없어 생성함");
-    fs.mkdirSync("poster");
-  }
-});
 
 const crawler = async () => {
   try {
-    const browser = await puppeteer.launch({
-      headless: process.env.NODE_ENV === "production",
-      args: ["--window-size=1920,1080"] // browser크기
-    });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.setViewport({
-      // 화면 크기
-      width: 1920,
-      height: 1920
-    });
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
-    );
-    add_to_sheet(ws, "C1", "s", "평점");
-    for (const [i, r] of records.entries()) {
-      await page.goto(r.링크);
-      const result = await page.evaluate(() => {
-        const scoreEl = document.querySelector(".score.score_left .star_score");
-        let score = "";
-        if (scoreEl) {
-          score = scoreEl.textContent;
+    await page.goto("https://unsplash.com");
+    let result = [];
+    while (result.length <= 30) {
+      const srcs = await page.evaluate(() => {
+        window.scrollTo(0, 0);
+        let imgs = [];
+        const imgEls = document.querySelectorAll("figure"); // 사이트 바뀌었을 때 클래스 적절히 바꾸기
+        if (imgEls.length) {
+          imgEls.forEach(v => {
+            let src = v.querySelector("img._2zEKz").src;
+            if (src) {
+              imgs.push(src);
+            }
+            v.parentElement.removeChild(v);
+          });
         }
-        const imgEl = document.querySelector(".poster img");
-        let img = "";
-        if (imgEl) {
-          img = imgEl.src;
-        }
-        return { score, img };
+        window.scrollBy(0, 100);
+        setTimeout(() => {
+          window.scrollBy(0, 200);
+        }, 500);
+        return imgs;
       });
-      if (result.score) {
-        const newCell = "C" + (i + 2);
-        add_to_sheet(ws, newCell, "n", parseFloat(result.score.trim()));
-      }
-      if (result.img) {
-        await page.screenshot({
-          path: `screenshot/${r.제목}.png`,
-          // fullpage: true,
-          clip: {
-            x: 100,
-            y: 100,
-            width: 300,
-            height: 300
-          }
-        });
-        const imgResult = await axios.get(
-          result.img.replace(/\?.*$/, ""), // 쿼리스트링(?부터 끝까지) 제거 정규표현식
-          {
-            responseType: "arraybuffer" //buffer가 연속적으로 들어있는 자료구조
-          }
-        );
-        fs.writeFileSync(`poster/${r.제목}.jpg`, imgResult.data);
-      }
-      await page.waitFor(1000);
+      result = result.concat(srcs);
+      await page.waitForSelector("figure");
     }
+    console.log(result);
     await page.close();
     await browser.close();
-    xlsx.writeFile(workbook, "xlsx/result.xlsx");
-  } catch (err) {
-    console.error(err);
+  } catch (e) {
+    console.error(e);
   }
 };
 
